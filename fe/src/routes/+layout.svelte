298392b1d2ca87684b222311d85cc1ea1a7a0c23
Jolvera2001 <jolvera6@stedwards.edu>
2023-12-25 23:03:16 -0600

    applied proper layouts

--- fe/src/routes/+layout.svelte
+++ fe/src/routes/+layout.svelte
@@ -2,24 +2,5 @@
     import '../app.css'
 </script>
 
-<style>
-	.transition-font {
-		transition: font-size 0.3s ease-in-out;
-	}
-</style>
-
-<div class="p-4 bg-pastelGreen text-greenText">
-	<div class="container mx-auto px-4">
-		<nav class="flex items-center justify-center h-16">
-			<div class="flex">
-				<a href="/" class="px-4 py-2 text-xl transition-font hover:text-2xl">Home</a>
-				<a href="/projects" class="px-4 py-2 text-xl transition-font hover:text-2xl">Projects/Experiences</a>
-				<a href="/" class="px-4 py-2 text-xl transition-font hover:text-2xl">Contact</a>
-				<a href="/blog" class="px-4 py-2 text-xl transition-font hover:text-2xl">Blog</a>
-			</div>
-		</nav>
-	</div>
-</div>
-
 <slot />
 
