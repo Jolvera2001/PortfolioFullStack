49c590062fd3b9ffa035ae43bb6806ffdd462002
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-02-04 16:51:41 -0600

    Redid all layouts

--- fe/src/routes/(contact)/+layout.svelte
+++ fe/src/routes/(contact)/+layout.svelte
@@ -12,7 +12,6 @@
 				<a href="/aboutme" class="px-4 py-2 text-xl transition-font hover:text-2xl">About Me</a>
 				<a href="/projects" class="px-4 py-2 text-xl transition-font hover:text-2xl">Projects/Experiences</a>
 				<a href="/blog" class="px-4 py-2 text-xl transition-font hover:text-2xl">Blog</a>
-				<a href="/contact" class="px-4 py-2 text-xl transition-font hover:text-2xl">Contact</a>
 			</div>
 		</nav>
 	</div>
