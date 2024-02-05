f38957d89363d307937dd30a6715b5c5289ce409
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-02-04 18:25:51 -0600

    rebuilt frontend

---
+++ be/PortfolioBackend/wwwroot/_app/immutable/nodes/8.876696bd.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.8b5adeb6.js";import{S as t,i as e}from"../chunks/index.d7cc7e7f.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
