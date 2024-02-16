10942cf46957f3165b0129b0a9e067eb202c41cc
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-16 16:35:13 -0600

    Successfully added in Exp and Proj Boxes

---
+++ fe/src/lib/ExpContainer.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let exp;
+
+</script>
+<div class="w-44 h-60 text-center">
+    <div class="border-4 rounded-xl border-solid w-44 h-60" />
+    <h1 class="text-xl">{exp.title}</h1>
+</div>
---
+++ fe/src/lib/ProjContainer.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let proj;
+
+</script>
+<div class="w-44 h-60 text-center">
+    <div class="border-4 rounded-xl border-solid w-44 h-60" />
+    <h1 class="text-xl">{proj.title}</h1>
+</div>
